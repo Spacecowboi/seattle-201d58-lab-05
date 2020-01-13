0ed4d25080b8cfdeacf2cd1651390844f9720bb9
spacecowboi <cunningham2192@gmail.com>
2020-01-12 20:47:10 -0800

    running into issues, going to scrap current iteration of function and try again

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -79,12 +79,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  var a = testArray[0];
+  var b = testArray[1];
+  var c = testArray[2];
+  var funArray = sum(a,b)[0];
+  var funArray2 = sum(funArray,c)[0];
+  console.log(funArray2);
+
+  return [funArray2, a + ',' + b + ',' + c + ' was passed in array of numbers ' + ' and ' + funArray2 + ' is their sum ' + '.'];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
